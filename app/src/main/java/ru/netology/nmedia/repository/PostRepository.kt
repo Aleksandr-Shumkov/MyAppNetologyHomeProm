43055d77883490d30ae4fa74e7d56d2e9922e8d1
Shumkov Aleksandr <aleksandr.shymkov@gmail.com>
2023-06-25 00:01:10 +0700

    HomeWork5_Work_Multithreading 1

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -4,10 +4,6 @@ import androidx.lifecycle.LiveData
 import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
-    /*fun getAll(): List<Post>
-    fun likeById(post: Post): Post
-    fun save(post: Post)
-    fun removeById(id: Long)*/
 
     fun getAllAsync(callback: GetCallback<List<Post>>)
     fun likeByIdAsync(post: Post, callback: GetCallback<Unit>)
